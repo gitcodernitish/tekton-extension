b195efd59214e0784cfc9596abfc6f91d6483a55
vijay.chintala <vijay.chintala@rubrik.com>
2021-03-15 03:37:25 +0530

    Add express dummy server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.get('/sample', (req, res) => res.send('Hello Tekton Dashboard !' + path.resolve(__dirname, '.'));
+app.get('/sample', (req, res) => res.send('Hello Tekton Dashboard !'));
 app.get('/bundle', (req, res) => res.sendFile(path.resolve(__dirname, './tekton-extension/src/index.js')));
 app.listen(3000, '0.0.0.0');
 
